e165413440c2bce9b0a4159c3017ffdbfa0c6aad
Nassim <chaabinassim@outlook.com>
2023-08-20 21:42:27 +0100

    polish static files

--- ecommerce/production.py
+++ ecommerce/production.py
@@ -23,7 +23,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
